9728c68eb08ce9e1911ad9b3d3ffd5460038adb7
GigaBerdzenishvili <gigushinio13@gmail.com>
2023-04-03 17:09:59 +0400

    navbar UI completed

---
+++ app/components/NavBar/Logo.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+
+  return (
+    <Image
+      className="logo cursor-pointer"
+      height="100"
+      width="100"
+      alt="Logo"
+      src="/images/logo.png"
+    />
+  );
+};
+
+export default Logo;
