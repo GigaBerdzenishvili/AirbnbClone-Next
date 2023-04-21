5222ae4d81b8e28bda48b3cc6b57cf508141dfc7
GigaBerdzenishvili <gigushinio13@gmail.com>
2023-04-21 13:13:47 +0400

    properties page done & delete properti func done

--- app/components/NavBar/UserMenu.tsx
+++ app/components/NavBar/UserMenu.tsx
@@ -91,7 +91,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   onClick={() => router.push("/reservations")}
                   label="My Reservations"
                 />
-                <MenuItem onClick={() => {}} label="My Properties" />
+                <MenuItem
+                  onClick={() => router.push("/properties")}
+                  label="My Properties"
+                />
                 <MenuItem onClick={rentModal.onOpen} label="Airbnb My Home" />
                 <MenuItem onClick={() => signOut()} label="Log Out" />
               </>
