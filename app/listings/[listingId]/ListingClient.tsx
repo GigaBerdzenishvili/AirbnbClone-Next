ad78465dcafdab7d61a044e238e19ce25d3cbc43
GigaBerdzenishvili <gigushinio13@gmail.com>
2023-04-21 18:34:08 +0400

    fixed selection range on mobile

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -108,7 +108,13 @@ const ListingClient: React.FC<ListingClientProps> = ({
             id={listing.id}
             currentUser={currentUser}
           />
-          <div className="grid grid-cols-3 md:grid-cols-7 md:gap-10 mt-6">
+          <div
+            className=" grid 
+              grid-cols-1 
+              md:grid-cols-7 
+              md:gap-10 
+              mt-6"
+          >
             <ListingInfo
               user={listing.user}
               category={category}
@@ -119,10 +125,10 @@ const ListingClient: React.FC<ListingClientProps> = ({
               locationValue={listing.locationValue}
             />
             <div
-              className=" order-first 
-                mb-10 
-                md:order-last 
-                md:col-span-3"
+              className="order-first 
+              mb-10 
+              md:order-last 
+              md:col-span-3"
             >
               <ListingReservation
                 price={listing.price}
